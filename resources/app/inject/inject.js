ad549265967f11e8fea629c79e4095512f8a9630
Smit Bhaskar Shetye <66062619+smitshetye@users.noreply.github.com>
2021-09-07 19:54:13 +0530

    Hasya-Katta-Official-win32-x64-Portable

---
+++ resources/app/inject/inject.js
@@ -0,0 +1,8 @@
+var data_tracking =  {
+      platform: "windows",
+      appID:"w9dqCHeT9ieZZbVP57Sq",
+      url:"https://smitshetye.github.io/",
+      appname:"Hasya Katta Official"
+    };
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push(data_tracking);(function (w, d, s, l, i) {  w[l] = w[l] || [];  w[l].push({  'gtm.start': new Date().getTime(), event: 'gtm.js', }); let f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? `&l=${l}` : ''; j.async = true; j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`; f.parentNode.insertBefore(j, f); }(window, document, 'script', 'dataLayer', 'GTM-N9XW463'));
